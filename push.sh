76262bc51313cd18417a07005b430e874886f410
sunnyingit <sunliwodewy@163.com>
2016-05-05 15:24:19 +0800

    push by shell

--- push.sh
+++ push.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
- cp -r ~/workspace/book/_book /tmp 
+# 使用 gitbook init 初始化书籍目录
+# 使用 gitbook serve 编译书籍， 然后就可以本地在本地访问gitbook
+# 修改之后 首先使用git add . git commit 提交本地的修改内容，然后执行push.sh脚本提交到远程分支上
+
+ cp -r ~/workspace/book/_book /tmp
 
  cd ~/workspace/book
 
  git checkout gh-pages
- 
+
  [ $? != 0 ] && echo "fuck dog, !! checkout gh-pages failed!!!" && exit
 
  cp -r /tmp/_book/* .
 
- ls -la 
+ ls -la
 
  git add -A  > /dev/null 2>&1
- 
+
  [ $? != 0 ] && echo "fuck dog git add failed" && git checkout master && exit
 
  git commit -m 'push by shell'
- 
+
  git push > /dev/null 2>&1
 
  [ $? != 0 ] && echo "fuck dog push sunny failed" && git checkout master && exit
