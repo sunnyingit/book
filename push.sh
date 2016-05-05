cbbf8e6555e0cb950c16632433e1d088a305d541
sunnyingit <sunliwodewy@163.com>
2016-05-05 14:41:51 +0800

    commit all update!

--- push.sh
+++ push.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
- cp -r ~/workspace/book/_book /tmp 
+# 使用 gitbook init 初始化书籍目录
+# 使用 gitbook serve 编译书籍， 然后就可以本地在本地访问gitbook
+
+ git add -A
+ git commit -m 'commit all update!'
+
+ cp -r ~/workspace/book/_book /tmp
 
  cd ~/workspace/book
 
  git checkout gh-pages
- 
+
  [ $? != 0 ] && echo "fuck dog, !! checkout gh-pages failed!!!" && exit
 
  cp -r /tmp/_book/* .
 
- ls -la 
+ ls -la
 
  git add -A  > /dev/null 2>&1
- 
+
  [ $? != 0 ] && echo "fuck dog git add failed" && git checkout master && exit
 
  git commit -m 'push by shell'
- 
+
  git push > /dev/null 2>&1
 
  [ $? != 0 ] && echo "fuck dog push sunny failed" && git checkout master && exit
